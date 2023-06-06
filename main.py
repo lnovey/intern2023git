2c6e0d4b582d0e1bc5b368170b361a3d5956413a
Luke Novey <lnoveygit@gmail.com>
2023-06-06 14:13:55 -0400

    add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import sys 
+
+def main() -> int:
+    """print args"""
+    print(sys.argv)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())  # next section explains the use of sys.exit
